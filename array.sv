6e7f5ac732f048d1011d877961cc535e3aea7985
Abhishek Shukla <108926031+shivashukla99@users.noreply.github.com>
2026-09-26 23:05:07 +0530

    Implement packed and unpacked array examples
    Added examples of packed and unpacked arrays in SystemVerilog.

--- array.sv
+++ array.sv
@@ -1,1 +1,47 @@
+// array - Fixed Array, Dynamic Size Array, 
+//Fixed array - Packed Array , Unpacked Array 
+
+//Packed Array  1d, 2d,3d
+
+//Unpacked Array 1d, 2d ,3d 
+
+module example;
+
+  //1d PA
+
+  reg [1:0]arr
+
+  initial 
+    begin
+
+      arr =2'b00;
+      arr = 4'b1231; //In this case one last two bit inserted into the array
+      
+      
+    end
+//2d PA
+
+  bit [1:0][2:0]arr
+  //3d PA
+
+  bit [1:0][2:0][3:0]arr;
+  
+    
+endmodule
+
+
+/// Unpacked Array - 1d 2d 3d 
+
+
+
+
+
+
+
+
+
+
+
+
+
 
